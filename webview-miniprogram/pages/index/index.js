6226623287435c4e43cea2e0f40dfd2a5dd38586
Jake <jake@example.com>
2026-04-19 20:50:41 +0700

    fix: add cache busting to JS files

--- webview-miniprogram/pages/index/index.js
+++ webview-miniprogram/pages/index/index.js
@@ -1,3 +1,11 @@
 Page({
-  data: {}
+  data: {
+    url: ''
+  },
+  onLoad: function () {
+    // 添加时间戳强制微信跳过缓存，获取最新的服务器前端代码
+    this.setData({
+      url: `http://8.215.108.239/index.html?t=${Date.now()}`
+    });
+  }
 })
